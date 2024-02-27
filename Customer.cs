50156e74dcdf056b4a65260dd3ac40d68042c0c4
Kasper Pelle Jensen <kaj007@edu.zealand.dk>
2024-02-27 13:31:45 +0100

    Add project files.

---
+++ Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaStore
+{
+    public class Customer
+    {
+        private string _name;
+        private string _email;
+        private string _phoneNr;
+
+        public Customer(string name, string email, string phoneNr)
+        { 
+            _name = name;
+            _email = email;
+            _phoneNr = phoneNr;
+        }
+        
+        public string Name 
+        { get { return _name; } }
+
+        public string Email
+        { get { return _email; } }
+
+        public string PhoneNr
+        { get { return _phoneNr; } }
+    }
+}
---
+++ Store.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaStore
+{
+    public class Store
+    {
+        private string _name;
+        private string _address;
+        private string _phoneNr;
+
+        public Store(string name, string address, string phoneNr) 
+        { 
+            _name = name;
+            _address = address;
+            _phoneNr = phoneNr;
+        }
+
+        public string Name
+        { get { return _name; } }
+
+        public string Address
+        { get { return _address; } }
+
+        public string PhoneNr 
+        { get { return _phoneNr;} }
+    }
+}
